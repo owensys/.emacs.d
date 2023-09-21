2243b98f5181ab6daa13a4d9bb6dfa338d4314de
owensys <owensys@hotmail.com>
2023-09-21 13:59:55 +0800

    update holo layer config

--- lisp/init-holo-layer.el
+++ lisp/init-holo-layer.el
@@ -9,12 +9,13 @@
                    (setq holo-layer-enable-cursor-animation t)
                    (setq holo-layer-hide-mode-line t)
                    (setq holo-layer-enable-window-border t)
+                   (setq holo-layer-inactive-window-color "gainsboro")
                    (setq holo-layer-enable-window-number-background t)
 
-		   (holo-layer-enable)
+		           (holo-layer-enable)
                    ;; (run-with-idle-timer 1 nil (lambda ()
-                                                ;; (fullscreen-toggle t)
-                                                ;; (holo-layer-enable)))
+                   ;; (fullscreen-toggle t)
+                   ;; (holo-layer-enable)))
                    ))
 
 ;; test code
